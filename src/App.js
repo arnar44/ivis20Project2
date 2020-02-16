0753b1b6d14e623651fbe39f210be08e31792d74
arnar44 <arnar44@gmail.com>
2020-02-16 17:31:23 +0100

    Cleanup React init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import NotFound from './routes/notFound/NotFound';
+
+
+
+/* todo fleiri routes */
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+
+    return (
+      <main className="main">
+        <Switch location={this.props.location}>
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/routes/notFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './NotFound.css';
+
+export default function NotFound() {
+
+    return (
+        <React.Fragment>
+
+            <div>
+                <p>Not Found</p>
+            </div>
+
+        </React.Fragment>
+    );
+}
